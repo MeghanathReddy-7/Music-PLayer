f0125dc9bbc3f4a59654c0c7a75d36d7b7633e46
MeghanathReddy <meghanathreddyallipurapu@gmail.com>
2024-04-24 17:58:34 +0530

    first commit

--- music-list.js
+++ music-list.js
@@ -1,62 +1,120 @@
 let allMusic = [
-    {
-        name: "Shape-of-You",
-        artist: "Ed Sheeran",
-        img: "music-1",
-        src: "music-1"
-    },
-    {
-        name: "Calm-Down",
-        artist: "Rema, Selena Gomez",
-        img: "music-2",
-        src: "music-2"
-    },
-    {
-        name: "Let Me Down Slowly",
-        artist: "Alec Benjamin",
-        img: "music-3",
-        src: "music-3"
-    },
-    {
-        name: "Love Me Like You Do",
-        artist: "Ellie Goulding",
-        img: "music-4",
-        src: "music-4"
-    },
-    {
-        name: "Starboy",
-        artist: "The Weeknd",
-        img: "music-5",
-        src: "music-5"
-    },
-    {
-        name: "Girls Like You",
-        artist: "Maroon 5",
-        img: "music-6",
-        src: "music-6"
-    },
-    {
-        name: "On My Way",
-        artist: "Alan Walker",
-        img: "music-7",
-        src: "music-7"
-    },
-    {
-        name: "Sorry",
-        artist: "Justin Beiber",
-        img: "music-8",
-        src: "music-8"
-    },
-    {
-        name: "Sugar and Brownies",
-        artist: "Dharia",
-        img: "music-9",
-        src: "music-9"
-    },
-    {
-        name: "Faded",
-        artist: "Alan Walker",
-        img: "music-10",
-        src: "music-10"
-    },
+  {
+    name: "Bharatha Vedamuga",
+    artist: "DSP",
+    img: "prabhas-1",
+    src: "prabhas-1",
+  },
+  {
+    name: "Muvvala Navvakala",
+    artist: "DSP",
+    img: "prabhas-1",
+    src: "prabhas-2",
+  },
+  {
+    name: "Paravaledhu",
+    artist: "Sekhar",
+    img: "Manasara",
+    src: "Manasara",
+  },
+  {
+    name: "Pranaamam",
+    artist: "DSP",
+    img: "ntr-2",
+    src: "ntr-2",
+  },
+  {
+    name: "Apple Beauty",
+    artist: "DSP",
+    img: "ntr-2",
+    src: "ntr-3",
+  },
+  {
+    name: "Nee Selvadigi",
+    artist: "DSP",
+    img: "ntr-2",
+    src: "ntr-4",
+  },
+  {
+    name: "Shape-of-You",
+    artist: "Ed Sheeran",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec Benjamin",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Girls Like You",
+    artist: "Maroon 5",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "On My Way",
+    artist: "Alan Walker",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "Sorry",
+    artist: "Justin Beiber",
+    img: "music-8",
+    src: "music-8",
+  },
+  {
+    name: "Faded",
+    artist: "Alan Walker",
+    img: "music-10",
+    src: "music-10",
+  },
+  {
+    name: "Prema desham",
+    artist: "Manisharma",
+    img: "ntr-1",
+    src: "ntr-1",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-1",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-2",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-3",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-4",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-5",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-6",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-7",
+  },
+  {
+    name: "Bahubali",
+    artist: "M M Keeravani",
+    src: "B-8",
+  },
 ];
